12a1663edb942aa2b65d13e14ecf34b66759eabb
cristian78p <ccreyb@gmail.com>
2025-03-23 21:42:19 -0500

    clienteFactory implementado

--- examen 2/ClienteFactory.cs
+++ examen 2/ClienteFactory.cs
@@ -8,6 +8,20 @@ namespace examen_2
 {
     public static class ClienteFactory
     {
-
+        public static Cliente CrearCliente(string tipo, string nombre, int identificacion, double saldoInicial, int cantidadCuentasActivas = 0 )
+        {
+            if (tipo == "Corporativo")
+            {
+                return new ClienteCorporativo(nombre, identificacion, saldoInicial);
+            }
+            else if (tipo == "Individual")
+            {
+                return new ClienteIndividual(nombre, identificacion, saldoInicial, cantidadCuentasActivas);
+            }
+            else
+            {
+                throw new ArgumentException("Tipo de cliente no válido");
+            }
+        }
     }
 }
